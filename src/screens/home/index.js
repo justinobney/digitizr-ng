2fd3db265524d9341427d710753f73336d410224
Justin Obney <justinobney@gmail.com>
2016-04-18 22:44:17 -0500

    refactor(home): removed un-needed stage component

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,11 +1,16 @@
 import styles from './index.scss';
 import template from './index.html';
 
-function HomeController(workflowService){
+function HomeController($element, workflowService){
   const home = this;
-  home.workflowConfig = workflowService.config;
 
+  home.workflowConfig = workflowService.config;
   home.jumpToWorkflow = jumpToWorkflow;
+
+  home.$postLink = () => {
+    const mount = $element[0].querySelector('.home-component-stage-content');
+    workflowService.registerStage(angular.element(mount));
+  }
 
   function jumpToWorkflow(key){
     workflowService.transition(key);
